accc14699b077543433ffc45cb5836cf2aa5a6fd
Haydart <makowiecki.radoslaw@gmail.com>
2018-12-01 11:03:22 +0100

    Add sample plate images

--- sample/test.py
+++ sample/test.py
@@ -2,7 +2,7 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('image.jpeg',0)
+img = cv2.imread('image.jpg',0)
 ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
 ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
 ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
@@ -13,8 +13,9 @@ titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
 images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
 
 for i in range(6):
-    plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
+    plt.subplot(2,3,i+1)
+    plt.imshow(images[i],'gray')
     plt.title(titles[i])
     plt.xticks([]),plt.yticks([])
 
-plt.savefig('processed.png')
+fig = plt.savefig('processed.png', figsize=(1920, 1080), dpi=360)
