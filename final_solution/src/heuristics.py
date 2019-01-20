1317e06fa37cfe66986e5ca428ce9825548a0394
lkopocinski <lkopocinski@gmail.com>
2019-01-20 17:28:08 +0100

    Heuristics tmp

--- final_solution/src/heuristics.py
+++ final_solution/src/heuristics.py
@@ -2,6 +2,7 @@ from copy import copy
 
 
 is_in_the_same_row = lambda ya, yb: (abs(ya -yb) <= 20)
+is_close_to = lambda x1a, x0b: (abs(x0b - x1a) <= 30)
 
 
 def join_separated(bands):
@@ -14,10 +15,27 @@ def join_separated(bands):
             if (is_in_the_same_row(y0a, y0b)) and (is_in_the_same_row(y1a, y1b)):
                 one_row_bands.append((y0b, y1b, x0b, x1b))
 
-        y0 = min([band[0] for band in one_row_bands])
-        y1 = max([band[1] for band in one_row_bands])
-        x0 = min([band[2] for band in one_row_bands])
-        x1 = max([band[3] for band in one_row_bands])
+        one_row_bands_new = []
+        if len(one_row_bands) >= 2:
+            for i in range(0, len(one_row_bands)-1, 1):
+                x1a = one_row_bands[i][3]
+                x0b = one_row_bands[i+1][2]
+
+                if is_close_to(x1a, x0b):
+                    one_row_bands_new.append(one_row_bands[i])
+                    one_row_bands_new.append(one_row_bands[i+1])
+                else:
+                    if one_row_bands[i] not in new_bands:
+                        new_bands.append(one_row_bands[i])
+                    if i == len(one_row_bands)-1:
+                        new_bands.append(one_row_bands[i+1])
+        else:
+            one_row_bands_new = one_row_bands
+
+        y0 = min([band[0] for band in one_row_bands_new])
+        y1 = max([band[1] for band in one_row_bands_new])
+        x0 = min([band[2] for band in one_row_bands_new])
+        x1 = max([band[3] for band in one_row_bands_new])
 
         new_bands.append((y0, y1, x0, x1))
 
