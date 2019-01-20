b33ed211811b21a143b9a2b7d458e09e83ade5db
Haydart <makowiecki.radoslaw@gmail.com>
2019-01-20 03:30:17 +0100

    Test Otsu binarization approach

--- recognize_license_plate.py
+++ recognize_license_plate.py
@@ -3,5 +3,5 @@ from pytesseract import pytesseract
 
 if __name__ == '__main__':
     print(pytesseract.get_tesseract_version())
-    print(pytesseract.image_to_data(Image.open('ocr.png')))
-    # print(pytesseract.image_to_data(Image.open('ocr-ready.jpg')))
+    # print(pytesseract.image_to_data(Image.open('ocr.png')))
+    print(pytesseract.image_to_data(Image.open('ocr-ready.jpg')))
