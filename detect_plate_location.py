11d1f5050efe5250ee1eb9b286c2930a34738b18
Haydart <makowiecki.radoslaw@gmail.com>
2019-01-20 21:14:59 +0100

    Fix old detect plate location pipeline

--- detect_plate_location.py
+++ detect_plate_location.py
@@ -1,4 +1,8 @@
+from util.image_display_helper import *
+from util.input_output import *
 from util.utils import *
+
+display = ImageDisplayHelper(True, 3, 5)
 
 if __name__ == '__main__':
     image = cv2.imread('dataset/license_plate_snapshots/test_002.jpg')
@@ -6,31 +10,32 @@ if __name__ == '__main__':
 
     grayscale_image = gray_scale(image)
 
-    plot_image(grayscale_image, 1, 'Original image grayscale')
-    plot_image(grayscale_image, 2, 'Original image grayscale BGR', fix_colors=False)
+    display.add_to_plot(grayscale_image, 1, 'Original image grayscale')
+    display.add_to_plot(grayscale_image, 2, 'Original image grayscale BGR', fix_colors=False)
 
     vertical_image = sobel_vertical_edge_detection(grayscale_image)
     skeletonized_image, _ = skeletonization(vertical_image)
-    plot_image(skeletonized_image, 3, 'Vertical Sobel -> skeletonization')
+    display.add_to_plot(skeletonized_image, 3, 'Vertical Sobel -> skeletonization')
 
     noise_removed_image = bilateral_filter(grayscale_image)
-    plot_image(noise_removed_image, 4, 'Bilateral filtering')
-    plot_image(noise_removed_image, 5, 'Bilateral filtering BGR', fix_colors=False)
+    display.add_to_plot(noise_removed_image, 4, 'Bilateral filtering')
+    display.add_to_plot(noise_removed_image, 5, 'Bilateral filtering BGR', fix_colors=False)
 
     vertical_image = sobel_vertical_edge_detection(noise_removed_image)
     skeletonized_vertical_edges_image = otsu_threshold(vertical_image)
-    plot_image(skeletonized_vertical_edges_image, 6, 'bilateral -> VSobel -> skeleton')
+    display.add_to_plot(skeletonized_vertical_edges_image, 6, 'bilateral -> VSobel -> skeleton')
 
     histogram_equalized_image = histogram_equalization(noise_removed_image)
-    plot_image(histogram_equalized_image, 7, 'Histogram equalization')
-    plot_image(histogram_equalized_image, 8, 'Histogram equalization BGR', fix_colors=False)
-    plot_image(canny_edge_detection(histogram_equalized_image), 9, 'Canny after histogram equalization')
+    display.add_to_plot(histogram_equalized_image, 7, 'Histogram equalization')
+    display.add_to_plot(histogram_equalized_image, 8, 'Histogram equalization BGR', fix_colors=False)
+    display.add_to_plot(canny_edge_detection(histogram_equalized_image), 9, 'Canny after histogram equalization')
 
     subtracted_image = morphological_opening(histogram_equalized_image)
-    plot_image(subtracted_image, 10, 'Opening subtracted')
+    display.add_to_plot(subtracted_image, 10, 'Opening subtracted')
     binarized_subtraction = otsu_threshold(subtracted_image)
-    plot_image(binarized_subtraction, 11, 'Subtraction -> otsu', fix_colors=False)
-    plot_image(otsu_threshold(sobel_vertical_edge_detection(binarized_subtraction)), 12, 'subtraction -> otsu -> sobel')
+    display.add_to_plot(binarized_subtraction, 11, 'Subtraction -> otsu', fix_colors=False)
+    display.add_to_plot(otsu_threshold(sobel_vertical_edge_detection(binarized_subtraction)), 12,
+                        'subtraction -> otsu -> sobel')
 
     plt.subplots_adjust(bottom=0.1, left=0.1, right=0.9, top=0.9, wspace=0.3, hspace=0.3)
 
