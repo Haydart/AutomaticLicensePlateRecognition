0bcf82b37ac22645e44d377aa53ecb7fb0d8ccdb
Haydart <makowiecki.radoslaw@gmail.com>
2019-01-26 17:52:49 +0100

    Add erosion experiments

--- temp.py
+++ temp.py
@@ -28,33 +28,41 @@ def _draw_plate_polygons(image, approximated_polygon):
     return cv2.drawContours(image, [approximated_polygon], -1, (randint(0, 255), randint(0, 255), randint(0, 255)), 3)
 
 
+def erosion(image, kernel_size=(3, 3), iterations=1):
+    kernel_size = np.ones(kernel_size, np.uint8)
+    return cv2.erode(image, kernel_size, iterations=iterations)
+
+
+
+
+
 # Loads the image then enhances it
-image = Image.open('dataset/skewed_trimmed_samples/skewed_009.jpg')
+image = Image.open('dataset/skewed_trimmed_samples/I00004.png')
 contrast = ImageEnhance.Contrast(image)
-img = contrast.enhance(3)
+img = contrast.enhance(2)
 img = np.asarray(img)
-r, g, b, a = cv2.split(img)
+r, g, b = cv2.split(img)
 contrast = cv2.merge([b, g, r])
 # Reads the enhanced image and converts it to grayscale, creates new file
 gray_image = cv2.cvtColor(contrast, cv2.COLOR_BGR2GRAY)  # there is a problem here
 
-# Adaptive Gaussian Thresholding
-th1 = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
 # Otsu's thresholding
 ret2, th2 = cv2.threshold(gray_image, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
 # Otsu's thresholding after Gaussian filtering
 blur = cv2.GaussianBlur(gray_image, (5, 5), 0)
 ret3, th3 = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
-_, result_polygon = _find_plate_contour(th3, img)
-polygon_image = _draw_plate_polygons(img, result_polygon)
 
-cv2.imshow("contrast bumped", img)
-cv2.imshow("grayscale after contrast", gray_image)
-cv2.imshow("adapt gaussian threshold", th1)
-cv2.imshow("otsu", th2)
-cv2.imshow("atsu after gaussian denoise", th3)
-cv2.imshow("approx polygon", polygon_image)
-cv2.waitKey()
+eroded = erosion(th3, iterations=5)
+_, result_polygon = _find_plate_contour(eroded, img)
+polygon_image = _draw_plate_polygons(img, result_polygon)
 
 # writes enhanced and thresholded img
 cv2.imwrite('temp3.png', th3)
+
+cv2.imshow("contrast bumped", img)
+cv2.imshow("grayscale after contrast", gray_image)
+cv2.imshow("otsu", th2)
+cv2.imshow("atsu after gaussian denoise", th3)
+cv2.imshow("erosion", eroded)
+cv2.imshow("approx polygon after erosion", polygon_image)
+cv2.waitKey()
