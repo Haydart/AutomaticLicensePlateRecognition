6a28185b509630f05d00d89669fb54d7ff52baa8
Haydart <makowiecki.radoslaw@gmail.com>
2019-01-27 19:51:26 +0100

    Play with tesseract params

--- recognize_license_plate.py
+++ recognize_license_plate.py
@@ -8,7 +8,7 @@ from util.basic_transformations import BasicTransformations
 from util.image_display_helper import ImageDisplayHelper
 
 if __name__ == '__main__':
-    dh = ImageDisplayHelper(True, 3, 20)
+    dh = ImageDisplayHelper(True, 2, 20)
     bt = BasicTransformations(dh)
     print(pytesseract.get_tesseract_version())
     dir_path = 'dataset/ocr_ready'
@@ -18,9 +18,8 @@ if __name__ == '__main__':
             image = cv2.imread('{}/{}'.format(dir_path, filename))
             gray_image = bt.gray_scale(image)
             binarized_image = bt.otsu_threshold(gray_image)
-            eroded_image = bt.erosion(binarized_image)
-            config = '-l eng --oem 3 --psm 10'
-            print(pytesseract.image_to_boxes(Image.fromarray(eroded_image), config=config))
+            config = '-l eng --oem 1 --psm 10'
+            print(pytesseract.image_to_string(Image.fromarray(gray_image), config=config))
 
     dh.plot_results()
 
