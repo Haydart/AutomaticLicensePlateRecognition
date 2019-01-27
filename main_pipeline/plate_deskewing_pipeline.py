5da986712e3fdaad626fc806f509c1304a938901
Haydart <makowiecki.radoslaw@gmail.com>
2019-01-27 02:39:53 +0100

    Create connected components playground

--- main_pipeline/plate_deskewing_pipeline.py
+++ main_pipeline/plate_deskewing_pipeline.py
@@ -26,24 +26,36 @@ def process(image_path):
     result_polygon = cf.find_plate_contours(binarized_image)
     print(result_polygon)
 
+    connected_components(binarized_image)
+
+    polygon_image = cf.draw_plate_polygon(img, result_polygon)
+    display_helper.add_to_plot(polygon_image, title="Approx polygon")
+
+    edges = cv2.Canny(gray_image, 50, 150, apertureSize=3)
+    min_line_length = 100
+    max_line_gap = 10
+    lines = cv2.HoughLinesP(edges, 1, np.pi / 180, 100, min_line_length, max_line_gap)
+    for x1, y1, x2, y2 in lines[0]:
+        cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    display_helper.add_to_plot(img, title="Hough Lines Prob")
+
+    display_helper.plot_results()
+    display_helper.reset_subplot()
+    print("DONE")
+
+
+def connected_components(binarized_image):
     eroded_image = bt.erosion(binarized_image)
-    ret, labels = cv2.connectedComponents(eroded_image)
-    label_hue = np.uint8(179 * labels / np.max(labels))
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(eroded_image, connectivity=4)
+    sizes = stats[:, -1]
+    print(sizes)
+    label_hue = np.uint8(179 * output / np.max(output))
     blank_ch = 255 * np.ones_like(label_hue)
     labeled_img = cv2.merge([label_hue, blank_ch, blank_ch])
     labeled_img = cv2.cvtColor(labeled_img, cv2.COLOR_HSV2BGR)
     # set bg label to black
     labeled_img[label_hue == 0] = 0
-
     display_helper.add_to_plot(labeled_img, title="Connected components")
-
-    polygon_image = cf.draw_plate_polygon(img, result_polygon)
-
-    display_helper.add_to_plot(polygon_image, title="Approx polygon")
-
-    display_helper.plot_results()
-    display_helper.reset_subplot()
-    print("DONE")
 
 
 if __name__ == '__main__':
