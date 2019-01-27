8ddedae867098d439250c3186b0acea2a4b84717
Haydart <makowiecki.radoslaw@gmail.com>
2019-01-27 05:13:15 +0100

    Choose most probable connected component and mask everything else in the image

--- main_pipeline/plate_deskewing_pipeline.py
+++ main_pipeline/plate_deskewing_pipeline.py
@@ -63,28 +63,36 @@ def connected_components(binarized_image):
     centroids_areas_no_black = np.delete(centroids_areas, 0, axis=0)
 
     # sort descending by size column
-    sorted_centroids_areas = centroids_areas_no_black[centroids_areas_no_black[:, -1].argsort()[::-1]]
-    largest_components_info = sorted_centroids_areas[:2, :]
+    sorted_components_info = centroids_areas_no_black[centroids_areas_no_black[:, -1].argsort()[::-1]]
 
-    image_center = binarized_image.shape[::-1]
+    largest_component_info = choose_plate_component(binarized_image, sorted_components_info)
+    largest_component_label = largest_component_info[0]
+    largest_components_image = np.zeros(output.shape)
+    largest_components_image[output == largest_component_label] = 255
+    display_helper.add_to_plot(largest_components_image, title="Connected components")
+
+
+def choose_plate_component(image, sorted_components_info):
+    largest_components_info = sorted_components_info[:2, :]
+    image_center = image.shape[::-1]
 
     def calculate_centroid_distance(row):
         import math
-
         x_dist = image_center[0] / 2 - row[1]
         y_dist = image_center[1] / 2 - row[2]
         row[3] = math.sqrt(pow(x_dist, 2) + pow(y_dist, 2))
         return row
 
-    np.apply_along_axis(calculate_centroid_distance, 1, largest_components_info)
-
-    largest_components_labels = largest_components_info[:, 0]
-
-    largest_components_image = np.zeros(output.shape)
-    for label in largest_components_labels:
-        largest_components_image[output == label] = 255
-
-    display_helper.add_to_plot(largest_components_image, title="Connected components")
+    # take largest component if it's much bigger than next largest
+    # otherwise, choose component, for which distance from image center to its centroid is the smallest
+    largest_component_area_trust_threshold = 4
+    largest_components_sizes = np.sort(largest_components_info[:, -1])
+    if largest_components_sizes[1] / largest_components_sizes[0] <= largest_component_area_trust_threshold:
+        np.apply_along_axis(calculate_centroid_distance, 1, largest_components_info)
+        # sort ascending by centroid-image center distance
+        return largest_components_info[largest_components_info[:, -2].argsort()][0, :]
+    else:
+        return largest_components_info[0, :]
 
 
 if __name__ == '__main__':
@@ -92,5 +100,5 @@ if __name__ == '__main__':
 
     dir_path = '../dataset/skewed_trimmed_samples/'
     for filename in os.listdir(dir_path):
-        if filename.startswith("I0000"):
+        if filename.startswith("I0001"):
             process('{}{}'.format(dir_path, filename))
