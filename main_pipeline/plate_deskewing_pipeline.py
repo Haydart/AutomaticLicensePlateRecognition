f830d0d3275235f287be2a72fce01b271649b90e
Haydart <makowiecki.radoslaw@gmail.com>
2019-01-27 05:45:46 +0100

    Create separate class for component extractor

--- main_pipeline/plate_deskewing_pipeline.py
+++ main_pipeline/plate_deskewing_pipeline.py
@@ -4,11 +4,13 @@ from cv2 import cv2
 
 from util.basic_transformations import BasicTransformations
 from util.image_display_helper import ImageDisplayHelper
+from util.plate_connected_component import PlateConnectedComponentExtractor
 from util.plate_contours import PlateContoursFinder
 
 cf = PlateContoursFinder()
 display_helper = ImageDisplayHelper(True, 2, 14)
 bt = BasicTransformations(display_helper)
+ex = PlateConnectedComponentExtractor(bt)
 
 
 def process(image_path):
@@ -24,11 +26,9 @@ def process(image_path):
     gray_image = bt.gray_scale(contrast_image)
     binarized_image = bt.binary_threshold(gray_image, 200)
 
-    plate_component_image = connected_components(binarized_image)
+    plate_component_image = ex.extract_plate_connected_component(binarized_image)
     display_helper.add_to_plot(plate_component_image, title="Plate connected component")
 
-    plate_component_image = cv2.normalize(src=plate_component_image, dst=None, alpha=0, beta=255,
-                                          norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8UC1)
     result_polygon = cf.find_plate_contours(plate_component_image)
     polygon_image = cf.draw_plate_polygon(img, result_polygon)
     display_helper.add_to_plot(polygon_image, title="Approx polygon")
@@ -48,54 +48,6 @@ def hough_lines(gray_image, img):
     for x1, y1, x2, y2 in lines[0]:
         cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
     display_helper.add_to_plot(img, title="Hough Lines Prob")
-
-
-def connected_components(binarized_image):
-    eroded_image = bt.erosion(binarized_image)
-    components_count, output, stats, centroids = cv2.connectedComponentsWithStats(eroded_image, connectivity=4)
-
-    sizes = stats[:, -1]
-    centroids_areas = np.column_stack((
-        np.arange(components_count, dtype=int),
-        centroids,
-        np.zeros(components_count),
-        sizes
-    ))
-
-    # delete background component
-    components_areas_centroids = np.delete(centroids_areas, 0, axis=0)
-    # sort components descending by size column
-    sorted_components_info = components_areas_centroids[components_areas_centroids[:, -1].argsort()[::-1]]
-
-    largest_component_info = choose_plate_component(binarized_image, sorted_components_info)
-    largest_components_image = np.zeros(output.shape)
-    # mask the image to only contain the chosen connected component
-    largest_components_image[output == largest_component_info[0]] = 255
-    return largest_components_image
-
-
-def choose_plate_component(image, sorted_components_info):
-    # take two largest components (plate might not be the largest one)
-    largest_components_info = sorted_components_info[:2, :]
-    image_center = image.shape[::-1]
-
-    def calculate_centroid_distance(row):
-        import math
-        x_dist = image_center[0] / 2 - row[1]
-        y_dist = image_center[1] / 2 - row[2]
-        row[3] = math.sqrt(pow(x_dist, 2) + pow(y_dist, 2))
-        return row
-
-    # take largest component if it's much bigger than second largest
-    # otherwise, choose component, for which distance from image center to its centroid is the smallest
-    largest_component_area_trust_threshold = 4
-    largest_components_sizes = np.sort(largest_components_info[:, -1])
-    if largest_components_sizes[1] / largest_components_sizes[0] <= largest_component_area_trust_threshold:
-        np.apply_along_axis(calculate_centroid_distance, 1, largest_components_info)
-        # sort ascending by centroid-image center distance
-        return largest_components_info[largest_components_info[:, -2].argsort()][0, :]
-    else:
-        return largest_components_info[0, :]
 
 
 if __name__ == '__main__':
